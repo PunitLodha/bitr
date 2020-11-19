bea547882e8b9ed0552f4f1d66ae9b59a01bd1ac
PunitLodha <punitlodha@pm.me>
2020-11-19 19:45:59 +0530

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::path::Path;
 
 extern crate serde;
 extern crate serde_bencode;
@@ -6,48 +6,20 @@ extern crate serde_bencode;
 extern crate serde_derive;
 extern crate serde_bytes;
 
-use serde_bencode::de;
-use serde_bytes::ByteBuf;
-
-#[derive(Debug, Deserialize)]
-struct Info {
-    name: String,
-    pieces: ByteBuf,
-    #[serde(rename = "piece length")]
-    piece_length: i64,
-    #[serde(default)]
-    length: Option<i64>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Torrent {
-    info: Info,
-    #[serde(default)]
-    announce: Option<String>, // can be replaced by announce list
-    #[serde(default)]
-    #[serde(rename = "creation date")]
-    creation_date: Option<i64>,
-    comment: Option<String>,
-    #[serde(default)]
-    #[serde(rename = "created by")]
-    created_by: Option<String>,
-}
-
-fn render_torrent(torrent: &Torrent) {
-    println!("name:\t\t{}", torrent.info.name);
-    println!("length:\t\t{:?}", torrent.info.length);
-    println!("piece length:\t{:?}", torrent.info.piece_length);
-    println!("announce:\t{:?}", torrent.announce);
-    println!("created by:\t{:?}", torrent.created_by);
-    println!("creation date:\t{:?}", torrent.creation_date);
-    println!("comment:\t{:?}", torrent.comment);
-}
+mod torrent;
+mod tracker;
+mod utils;
+use crate::torrent::Torrent;
 
 fn main() {
-    let contents = fs::read("../kubuntu-20.10-desktop-amd64.iso.torrent")
-        .expect("Something went wrong while reading the file");
-    match de::from_bytes::<Torrent>(&contents) {
-        Ok(t) => render_torrent(&t),
-        Err(e) => println!("ERROR: {:?}", e),
-    }
+    // path of the torrent file
+    // TODO  change this so that it can be taken as an argument
+    let path = Path::new("../kubuntu-20.10-desktop-amd64.iso.torrent");
+
+    let torrent = Torrent::new(&path);
+    println!("{}", &torrent);
+    let url = torrent.generate_tracker_url();
+    println!("{}", url.as_str());
+
+    tracker::send_tracker_request(url);
 }
