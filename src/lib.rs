9d6c1fa9f095b6023a1d290d5196b71f20040ead
PunitLodha <punitlodha@pm.me>
2020-11-29 10:35:26 +0530

    refactor

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use std::error::Error;
+use std::path::PathBuf;
+
+#[macro_use]
+extern crate serde_derive;
+
+mod peer;
+mod torrent;
+mod tracker;
+mod utils;
+
+use crate::torrent::Torrent;
+
+// create an alias for the result type
+pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+struct Client {
+    path: PathBuf,
+    peer_id: Vec<u8>,
+}
+
+impl Client {
+    fn new(file_path: String) -> Result<Client> {
+        // path of the torrent file
+        let path = PathBuf::from(file_path);
+        // generate the peer id
+        let peer_id = utils::generate_peer_id()?;
+        Ok(Client { path, peer_id })
+    }
+}
+
+pub fn run() -> Result<()> {
+    let file_path = std::env::args()
+        .nth(1)
+        .ok_or("path to torrent file is missing\nUsage: bitr <path to torrent file>")?;
+    let client = Client::new(file_path)?;
+    let torrent = Torrent::new(&client.path)?;
+    println!("{}", &torrent);
+
+    let url = torrent.generate_tracker_url(&client.peer_id)?;
+    println!("{}", url.as_str());
+
+    let res = tracker::send_tracker_request(url)?;
+
+    // todo change this
+    let peer = res.peers.iter().next().unwrap();
+    peer::connect_to_peer(peer, &torrent.info_hash, &client.peer_id)?;
+
+    Ok(())
+    /* for peer in res.peers.iter() {
+        peer::connect_to_peer(peer, &torrent.info_hash, &peer_id);
+    } */
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
-use std::path::Path;
-
-#[macro_use]
-extern crate serde_derive;
-
-mod torrent;
-mod tracker;
-mod utils;
-
-use crate::torrent::Torrent;
+use bitr::run;
+use std::process;
 
 fn main() {
-    // path of the torrent file
-    // TODO  change this so that it can be taken as an argument
-    let path = Path::new("../kubuntu-20.10-desktop-amd64.iso.torrent");
-
-    let torrent = Torrent::new(&path);
-    println!("{}", &torrent);
-
-    let peer_id = utils::generate_peer_id();
-
-    let url = torrent.generate_tracker_url(&peer_id);
-    println!("{}", url.as_str());
-
-    tracker::send_tracker_request(url);
+    if let Err(e) = run() {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
 }
