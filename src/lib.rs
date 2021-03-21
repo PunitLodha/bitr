5c937f9467a0c0a1da501172b6634358b0038c9e
PunitLodha <punitlodha@pm.me>
2021-03-21 14:49:05 +0530

    spawn disk manager and listen for pieces

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use tokio::sync::mpsc;
 #[macro_use]
 extern crate serde_derive;
 
+mod disk;
 mod manager;
 mod message;
 mod peer;
@@ -11,7 +12,7 @@ mod torrent;
 mod tracker;
 mod utils;
 
-use manager::{Command, Manager};
+use manager::{Command, DownloadedPiece, Manager};
 
 // create an alias for the result type
 pub type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
@@ -28,16 +29,23 @@ pub async fn run() -> Result<()> {
 
     // create mpsc channel for communication between piece picker and all peers
     let (send_to_manager, receive_from_peers) = mpsc::unbounded_channel::<Command>();
+    // create mpsc channel for communication between piece picker and disk manager
+    let (send_to_disk_manager, receive_pieces) = mpsc::unbounded_channel::<DownloadedPiece>();
     // spawn a new tokio task for each peer
     let handles = manager.connect_to_peers(res, send_to_manager);
 
-    let mut piece_picker = manager.spawn_piece_picker();
+    let mut piece_picker = manager.spawn_piece_picker(send_to_disk_manager);
+
+    let disk_manager = manager.spawn_disk_manager(receive_pieces)?;
+    let disk_handle = disk_manager.listen_for_pieces();
     // listen on mpsc channel for different commands from the peers
     piece_picker.listen_to_commands(receive_from_peers).await;
 
     for handle in handles {
         handle.await?;
     }
+
+    disk_handle.await?;
 
     Ok(())
 }
